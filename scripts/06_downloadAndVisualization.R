0a2c34d2000f8312ac8110d8261ee0388f3401df
Alan <alan.lorenzetti@gmail.com>
2020-12-30 00:41:45 -0300

    updated report

--- scripts/06_downloadAndVisualization.R
+++ scripts/06_downloadAndVisualization.R
@@ -4,9 +4,9 @@
 #' date: "Dec 30th, 2020"
 #' ---
 
-#' Download the non-redundant (pseudo)transcriptome of _Halobacterium salinarum_ NRC-1 (here)[linktogithub].
+#' Download the non-redundant (pseudo)transcriptome of _Halobacterium salinarum_ NRC-1 (here)[https://alanlorenzetti.github.io/halo_nr_tx/data/Hsalinarum_nrtx.fa].  
 
-#' Download the dictionary file (here)[placeholder].
+#' Download the dictionary file (here)[https://alanlorenzetti.github.io/halo_nr_tx/data/dictionary.tsv].  
 
 #' An interactive and searchable version of the locus_tag dictionary is available below
 datatable(dict)
