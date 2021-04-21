abc95c636c578afa475cbd07231688f72468d026
Alan Lorenzetti <alan.lorenzetti@gmail.com>
2021-04-21 09:17:23 -0300

    changed the interactive table interface adding download buttons

--- scripts/07_downloadAndVisualization.R
+++ scripts/07_downloadAndVisualization.R
@@ -6,19 +6,27 @@
 #'
 #' Download the non-redundant (pseudo)transcriptome of _Halobacterium salinarum_ NRC-1 [here](https://alanlorenzetti.github.io/halo_nr_tx/data/Hsalinarum_nrtx.fa).  
 #'
-#' Download the dictionary file [here](https://alanlorenzetti.github.io/halo_nr_tx/data/dictionary.tsv).  
+#' Download the dictionary file [here](https://alanlorenzetti.github.io/halo_nr_tx/data/dictionary.tsv) or clicking on the buttons above the interactive table.  
 #'
-#' Download the [COG (2020)](https://www.ncbi.nlm.nih.gov/research/cog) data for this non-redundant transcriptome [here](https://alanlorenzetti.github.io/halo_nr_tx/data/cog.tsv).  
+#' Download the [COG (2020)](https://www.ncbi.nlm.nih.gov/research/cog) data for this non-redundant transcriptome [here](https://alanlorenzetti.github.io/halo_nr_tx/data/cog.tsv) or clicking on the buttons above the interactive table.  
 #'
 #' ## Browsable tables {.tabset}
 #' ### Locus tag dictionary
 #' <div style = "width:100%; height: auto; margin: auto;">
 #+ echo=FALSE
-datatable(dict, options = list(scrollX = "800px"))
+datatable(dict, rownames = F,
+          extensions = "Buttons",
+          options = list(scrollX = "800px",
+                         buttons = c("copy", "csv", "excel"),
+                         dom = "Blfrtip"))
 #' </div>
 #'
 #' ### COG Info
 #' <div style = "width:100%; height: auto; margin: auto;">
 #+ echo=FALSE
-datatable(cogdict, options = list(scrollX = "800px"))
+datatable(cogdict, rownames = F,
+          extensions = "Buttons",
+          options = list(scrollX = "800px",
+                         buttons = c("copy", "csv", "excel"),
+                         dom = "Blfrtip"))
 #' </div>
